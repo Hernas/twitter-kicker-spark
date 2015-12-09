bf2c4319dfc00cfb4f08cc84322ef6cc2b7dc243
Michał Hernas <michal@hernas.pl>
2015-12-09 23:36:36 +0100

    Counting goals and publishing

--- app.ino
+++ app.ino
@@ -2,6 +2,14 @@
 #define LIGHT_SENSOR D5
 #define LED D7
 
+int goalsA = 0;
+bool goalStateA = false;
+
+int goalsB = 0;
+bool goalStateB = false;
+
+char publishHitString[3];
+
 void setup()
 {
   Serial.begin(9600);
@@ -11,23 +19,47 @@ void setup()
 void loop()
 {
   bool hit = false;
-	if(digitalRead(LIGHT_SENSOR) == LOW)
+  bool publish = false;
+
+  bool lightSensorOff = digitalRead(LIGHT_SENSOR) == LOW;
+	if(lightSensorOff)
 	{
+    // HIT
+    goalStateA = true;
     hit = true;
 	}
-
-
-	if(digitalRead(SENSOR) == LOW)
-	{
-    hit = true;
-	}
-
-  if(hit) {
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(LED, LOW);
+  if(!lightSensorOff && goalStateA) {
+    // So we got Sensor HIGH and now it went LOW, mean goal was registered
+    ++goalsA;
+    publish = true;
+    goalStateA = false;
   }
 
+
+  bool sensorOff = digitalRead(SENSOR) == LOW;
+	if(sensorOff)
+	{
+    // HIT
+    goalStateB = true;
+    hit = true;
+	}
+  if(!sensorOff && goalStateB) {
+    // So we got Sensor HIGH and now it went LOW, mean goal was registered
+    ++goalsB;
+    publish = true;
+    goalStateB = false;
+  }
+
+  if(hit) {
+    digitalWrite(LED, LOW);
+  } else {
+    digitalWrite(LED, HIGH);
+  }
+
+  if(publish) {
+    sprintf(publishHitString, "%d,%d", goalsA, goalsB);
+    Particle.publish("hit", publishHitString);
+  }
 }
 void pinsInit()
 {
