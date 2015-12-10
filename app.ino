12ffe0cfa381d1f9bbfccdaf94b6db7a8dff3ab1
Michał Hernas <michal@hernas.pl>
2015-12-10 09:29:28 +0100

    Finished

--- app.ino
+++ app.ino
@@ -2,6 +2,9 @@
 #define LIGHT_SENSOR D5
 #define LED D7
 
+#define GOAL_A_ADDRESS 10
+#define GOAL_B_ADDRESS 11
+
 int goalsA = 0;
 bool goalStateA = false;
 
@@ -9,11 +12,26 @@ int goalsB = 0;
 bool goalStateB = false;
 
 char publishHitString[3];
+int resetAll(String command);
 
 void setup()
 {
   Serial.begin(9600);
  	pinsInit();
+
+  Particle.function("resetAll", resetAll);
+
+  /*goalsA = EEPROM.read(GOAL_A_ADDRESS);
+  goalsB = EEPROM.read(GOAL_B_ADDRESS);*/
+}
+
+int resetAll(String command)
+{
+  goalsA = 0;
+  /*EEPROM.write(GOAL_A_ADDRESS, goalsA);*/
+
+  goalsB = 0;
+  /*EEPROM.write(GOAL_B_ADDRESS, goalsB);*/
 }
 
 void loop()
@@ -31,6 +49,7 @@ void loop()
   if(!lightSensorOff && goalStateA) {
     // So we got Sensor HIGH and now it went LOW, mean goal was registered
     ++goalsA;
+    /*EEPROM.write(GOAL_A_ADDRESS, goalsA);*/
     publish = true;
     goalStateA = false;
   }
@@ -46,6 +65,7 @@ void loop()
   if(!sensorOff && goalStateB) {
     // So we got Sensor HIGH and now it went LOW, mean goal was registered
     ++goalsB;
+    /*EEPROM.write(GOAL_B_ADDRESS, goalsB);*/
     publish = true;
     goalStateB = false;
   }
